916d389188acd920e3c482aa2954a5feb21aa527
Mitchell Simoens <325473+mitchellsimoens@users.noreply.github.com>
2025-02-01 08:53:27 -0500

    some login error handling

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { startAuthentication } from '@simplewebauthn/browser';
+import { useNavigate } from 'react-router-dom';
 
 const API_URL = 'http://localhost:3000';
 
@@ -7,28 +8,51 @@ export default function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [otp, setOtp] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const navigate = useNavigate();
+
+  const handleSuccess = () => {
+    setErrorMessage('');
+
+    navigate('/');
+  };
 
   const login = async () => {
-    const { options } = await fetch(`${API_URL}/login`, {
-      method: 'POST',
-      credentials: 'include',
-      body: JSON.stringify({ username, password }),
-      headers: { 'Content-Type': 'application/json' },
-    }).then((res) => res.json());
-
-    if (options) {
-      const response = await startAuthentication(options);
-
-      // TODO: check if this was successful and if not, fail login
-      await fetch(`${API_URL}/login/verify`, {
+    try {
+      const loginResponse = await fetch(`${API_URL}/login`, {
         method: 'POST',
         credentials: 'include',
-        body: JSON.stringify({ username, response, otp }),
+        body: JSON.stringify({ username, password, otp }),
         headers: { 'Content-Type': 'application/json' },
       });
-    }
 
-    alert('Login successful');
+      if (!loginResponse.ok) {
+        throw new Error('Verification failed');
+      }
+
+      const { options } = await loginResponse.json();
+
+      if (options) {
+        const response = await startAuthentication(options);
+
+        const verifyResponse = await fetch(`${API_URL}/login/verify`, {
+          method: 'POST',
+          credentials: 'include',
+          body: JSON.stringify({ username, response, otp }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (!verifyResponse.ok) {
+          throw new Error('Verification failed');
+        }
+
+        handleSuccess();
+      } else {
+        handleSuccess();
+      }
+    } catch {
+      setErrorMessage('Login failed. Please check your login information.');
+    }
   };
 
   return (
@@ -38,6 +62,7 @@ export default function Login() {
       <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
       <input type="text" placeholder="OTP (if required)" value={otp} onChange={(e) => setOtp(e.target.value)} />
       <button onClick={login}>Login</button>
+      {errorMessage && <div>{errorMessage}</div>}
     </div>
   );
 }
